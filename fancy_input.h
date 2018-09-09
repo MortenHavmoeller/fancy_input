a8e7ccd6e199b1962f2101665a0e102b2e671628
MortenHavmoeller <MortenHavmoeller@users.noreply.github.com>
2018-09-09 23:58:42 +0200

    first pass on fancy_input.h

--- fancy_input.h
+++ fancy_input.h
@@ -3,21 +3,46 @@
 #ifndef FANCY_INPUT_H
 #define FANCY_INPUT_H
 
+#include "object.h"
+#include "list.h"
+#include "rid.h"
+#include "set.h"
+#include "variant.h"
+#include "os/thread.h"
+#include "os/mutex.h"
+#include "os/input.h"
+
+
 #include "reference.h"
 
-class FancyInput : public Reference {
-	GDCLASS(FancyInput, Reference);
+class FancyInput : public Object {
+	GDCLASS(FancyInput, Object);
 	
-	int count;
+	static FancyInput *singleton;
+	static void thread_func(void *p_udata);
+	
+private:
+	bool thread_exited;
+	mutable bool exit_thread;
+	Thread *thread;
+	Mutex *mutex;
+	
+public:
+	static FancyInput *get_singleton();
+	Error init();
+	void lock();
+	void unlock();
+	void finish();
 	
 protected:
 	static void _bind_methods();
 	
-public:
-	void poke();
-	int get_count() const;
+private:
+	// things with RID...
 	
-	FancyInput();
+public:
+	// functions to be called from Godot...
+	
 };
 
 #endif
